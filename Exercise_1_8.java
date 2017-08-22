c72163d68877e779218babd9d073f5c269918366
Blaine-Rob5000 <Blaine.Rob5000@stu.stech.edu>
2017-08-22 12:19:10 -0600

    Add files via upload

---
+++ Exercise_1_8.java
@@ -0,0 +1,12 @@
+public class Exercise_1_8 {
+	public static void main(String[] args) {
+		System.out.println("Circle Radius:  5.5");
+		System.out.println("");
+		System.out.println("Area:");
+		System.out.println(Math.PI * 5.5 * 5.5);
+		System.out.println("");
+		System.out.println("Circumference:");
+		System.out.println(2 * Math.PI * 5.5);
+		System.out.println("");
+	}
+}
